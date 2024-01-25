a90eb21e735303d116599afaeb2c4ac3bf20fc8e
USER-7-C1 <arniscailunga3gmail.com>
2024-01-25 15:56:30 +0200

    initialisation de tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ import Photo from './Photo';
 
 
 
- 
+
 
 
 
@@ -34,59 +34,93 @@ export default function App() {
   return (
     <>
 
-    <div>
       <div>
-        <h3>Home</h3>
-        <Icones urlImg={iconTimeline} altImg="Timeline"/>
-        <div />
         <div>
-          <Icones urlImg={profilePhoto} />
-          <input type="text"  placeholder="What's happening?"/>
-          
-          <Mybutton name="Twitter" />
-          <Icones urlImg={icoMedia} altImg="Media"/>
-          <Icones urlImg={iconGif} altImg="Gif"/>
-          <Icones urlImg={iconPoll} altImg="Poll"/>
-          <Icones urlImg={iconEmoji} altImg="Emoji"/>
-          <Icones urlImg={iconSchedule} altImg="Schedule"/>
-            
-          <Icones urlImg={iconTwitter} altImg="Twitter"/>
+          <div className='header'>
+            <h3>Home</h3>
+            <Icones urlImg={iconTimeline} altImg="Timeline" />
+          </div>
+          <div>
+            <div className='profilphoto'>
+            <Icones urlImg={profilePhoto} />
+            </div>
+            <div>
+            <input type="text" placeholder="What's happening?" />
+            <div className='icones'>
+            <Mybutton name="Twitter" />
+            <Icones urlImg={icoMedia} altImg="Media" />
+            <Icones urlImg={iconGif} altImg="Gif" />
+            <Icones urlImg={iconPoll} altImg="Poll" />
+            <Icones urlImg={iconEmoji} altImg="Emoji" />
+            <Icones urlImg={iconSchedule} altImg="Schedule" />
+            <Icones urlImg={iconTwitter} altImg="Twitter" />
+            </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  <div>
-    <h5>CNN</h5>
-    <Icones urlImg={iconVerified} altImg="Verified"/>
-</div>
-<Paragraphe name="presidentJoe biden touted a new agreement reached witch
+      <div>
+        <div className='Paragraphe' bg-black >
+          </div>
+          
+        <h5>CNN</h5>
+        <Icones urlImg={iconVerified} altImg="Verified" />
+      </div>
+      <Paragraphe name="presidentJoe biden touted a new agreement reached witch
  the European Union to ease Trump-era tariffs on aluminum and steel as a
  major reakthrounghthat would serve to both sthrendgthen the US steel industry 
  and combat the global climate  crisis" />
- <Photo urlim={iconPhoto} />
-<div>
-        < Icones urlImg={iconReply} altImg="Reply"/>
-         <Icones urlImg={iconRetweet} altImg="Retweet"/>
-        <Icones urlImg={iconReact} altImg="React"/>
-        <Icones urlImg={iconShare} altImg="Share"/>
-         
-</div>
-<div>
-  <h5>the Nwe York Times</h5>
-  <Icones urlImg={iconVerified} altImg="Verified"/>
-</div>
-<div>
- < Paragraphe name="gardening boomed during the pandemic.Six Black writers
+      <Photo urlim={iconPhoto} />
+      <div>
+        < Icones urlImg={iconReply} altImg="Reply" />
+        <Icones urlImg={iconRetweet} altImg="Retweet" />
+        <Icones urlImg={iconReact} altImg="React" />
+        <Icones urlImg={iconShare} altImg="Share" />
+      </div>
+      <div>
+        <h5>the Nwe York Times</h5>
+        <Icones urlImg={iconVerified} altImg="Verified" />
+      </div>
+      <div>
+        <div className='Paragraphe2'>
+        </div>
+        < Paragraphe name="gardening boomed during the pandemic.Six Black writers
    share how has helping themre-establish, and reimagine,
     connection to cultivation and the land"/>
-   
-</div>
+
+      </div>
+      <div>
+        <Icones urlImg={iconReply} altImg="Reply" />
+        <Icones urlImg={iconRetweet} altImg="Retweet" />
+        <Icones urlImg={iconReact} altImg="React" />
+        <Icones urlImg={iconShare} altImg="Share" />
+      </div>
+      <div>
+        <h5>Twitter</h5>
+
+  
 <div>
-  <Icones urlImg={iconReply} altImg="Reply"/>
-  <Icones urlImg={iconRetweet} altImg="Retweet"/>
-  <Icones urlImg={iconReact} altImg="React"/>
-  <Icones urlImg={iconShare} altImg="Share"/>
+  <div>
+  </div>
+
+
+<Icones urlImg={iconReply} altImg="Reply" />
+        <Icones urlImg={iconRetweet} altImg="Retweet" />
+        <Icones urlImg={iconReact} altImg="React" />
+        <Icones urlImg={iconShare} altImg="Share" />
+
+  </div>
 </div>
-  </>
+<h5>Tweeter</h5>
+<Icones urlImg={iconReply} altImg="Reply" />
+        <Icones urlImg={iconRetweet} altImg="Retweet" />
+        <Icones urlImg={iconReact} altImg="React" />
+        <Icones urlImg={iconShare} altImg="Share" />
+
+<div>
+        <div/>
+      </div>
+    </>
 
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [], content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
